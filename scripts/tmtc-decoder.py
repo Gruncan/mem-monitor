05fcdd4468c66ddea6801455cc86ec895140f4be
Duncan Jones <dj223@outlook.com>
2024-12-27 18:50:56 +0000

    Added time decoding

--- scripts/tmtc-decoder.py
+++ scripts/tmtc-decoder.py
@@ -1,141 +1,187 @@
 
+from datetime import datetime, timedelta
+from pprint import pprint
 
 
+def format_timestamp(func):
+    def _wrapper(cls, *args, **kwargs):
+        if not isinstance(cls, AllocationPoint):
+            raise Exception("Can't format timestamp for unknown class")
+        return cls._format_timestamp() + func(cls, *args, **kwargs)
+    return _wrapper
 
-class Malloc:
-    def __init__(self, ptr, size):
+class AllocationPoint:
+
+    def __init__(self, timestamp):
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}: {getattr(self, "ptr")}"
+
+    def _format_timestamp(self):
+        return f"[{self.timestamp.strftime("%Y-%m-%d %H:%M:%S.%f")}] "
+
+class Malloc(AllocationPoint):
+    def __init__(self, timestamp, ptr, size):
+        super().__init__(timestamp)
         self.ptr = hex(ptr)
         self.size = size
 
+    @format_timestamp
     def __str__(self):
         return f"Malloc({self.size}) = {self.ptr}"
 
 
-class Calloc:
-    def __init__(self, nmemb, size, ptr):
+class Calloc(AllocationPoint):
+    def __init__(self, timestamp, nmemb, size, ptr):
+        super().__init__(timestamp)
         self.nmemb = nmemb
         self.size = size
         self.ptr = hex(ptr)
 
+    @format_timestamp
     def __str__(self):
         return f"Calloc({self.nmemb}, {self.size}) = {self.ptr}"
 
-class ReAlloc:
-    def __init__(self, ptr, size, new_ptr):
+class ReAlloc(AllocationPoint):
+    def __init__(self, timestamp, ptr, size, new_ptr):
+        super().__init__(timestamp)
         self.ptr = hex(ptr)
         self.size = size
         self.new_ptr = hex(new_ptr)
 
+    @format_timestamp
     def __str__(self):
         return f"Realloc({self.ptr}, {self.size}) = {self.new_ptr}"
 
-class ReAllocArray:
-    def __init__(self, ptr, nmemb, size, new_ptr):
+class ReAllocArray(AllocationPoint):
+    def __init__(self, timestamp, ptr, nmemb, size, new_ptr):
+        super().__init__(timestamp)
         self.ptr = hex(ptr)
         self.nmemb = nmemb
         self.size = size
         self.new_ptr = hex(new_ptr)
 
+    @format_timestamp
     def __str__(self):
         return f"Reallocarray({self.ptr}, {self.nmemb}, {self.size}) = {self.new_ptr}"
 
-class Free:
-    def __init__(self, ptr):
+class Free(AllocationPoint):
+    def __init__(self, timestamp, ptr):
+        super().__init__(timestamp)
         self.ptr = hex(ptr)
 
+    @format_timestamp
     def __str__(self):
         return f"Free({self.ptr})"
 
-class New:
-    def __init__(self, ptr, size):
+class New(AllocationPoint):
+    def __init__(self, timestamp, ptr, size):
+        super().__init__(timestamp)
         self.ptr = hex(ptr)
         self.size = size
 
+    @format_timestamp
     def __str__(self):
         return f"New({self.size}) = {self.ptr}"
 
 
 class NewNoThrow(New):
 
+    @format_timestamp
     def __str__(self):
         return f"New_nothrow({self.size}) = {self.ptr}"
 
 class NewArray(New):
 
+    @format_timestamp
     def __str__(self):
         return f"New[]({self.size}) = {self.ptr}"
 
 
 class NewArrayNoThrow(New):
 
+    @format_timestamp
     def __str__(self):
         return f"New[]_nothrow({self.size}) = {self.ptr}"
 
-class Delete:
+class Delete(AllocationPoint):
 
-    def __init__(self, ptr):
+    def __init__(self, timestamp, ptr):
+        super().__init__(timestamp)
         self.ptr = hex(ptr)
 
+    @format_timestamp
     def __str__(self):
         return f"Delete({self.ptr})"
 
-class DeleteSized:
+class DeleteSized(AllocationPoint):
 
-    def __init__(self, ptr, size):
+    def __init__(self, timestamp, ptr, size):
+        super().__init__(timestamp)
         self.ptr = hex(ptr)
         self.size = size
 
+    @format_timestamp
     def __str__(self):
         return f"Delete_sized({self.ptr}, {self.size})"
 
 
 class DeleteNoThrow(Delete):
 
+    @format_timestamp
     def __str__(self):
         return f"Delete_nothrow({self.ptr})"
 
 
 class DeleteArray(Delete):
 
+    @format_timestamp
     def __str__(self):
         return f"Delete[]({self.ptr})"
 
 class DeleteArraySized(DeleteSized):
 
+    @format_timestamp
     def __str__(self):
         return f"Delete[]_sized({self.ptr}, {self.size})"
 
 class DeleteArrayNoThrow(DeleteArray):
 
+    @format_timestamp
     def __str__(self):
         return f"Delete[]_nothrow({self.ptr})"
 
 class NewAligned(New):
 
-    def __init__(self, ptr, size, align):
-        super().__init__(ptr, size)
+    def __init__(self, timestamp, ptr, size, align):
+        super().__init__(timestamp, ptr, size)
         self.align = align
 
+    @format_timestamp
     def __str__(self):
         return f"New_align({self.size}, {self.align}) = {self.ptr}"
 
 class NewArrayAlign(NewAligned):
 
+    @format_timestamp
     def __str__(self):
         return f"New[]_align({self.size}, {self.align}) = {self.ptr}"
 
 
 class DeleteAlign(Delete):
 
-    def __init__(self, ptr, align):
-        super().__init__(ptr)
+    def __init__(self, timestamp, ptr, align):
+        super().__init__(timestamp, ptr)
         self.align = align
 
+    @format_timestamp
     def __str__(self):
         return f"Delete_align({self.ptr}, {self.align})"
 
 class DeleteArrayAlign(DeleteAlign):
 
+    @format_timestamp
     def __str__(self):
         return f"Delete[]_align({self.ptr}, {self.align})"
 
@@ -154,17 +200,30 @@ class TMtcDecoder:
         with open(self.filename, "rb") as f:
             self.content = f.read()
 
-    def decode(self):
+    def __decode_time(self, time):
+        tv_sec = ((time[0] << 24)
+                    | (time[1] << 16)
+                    | (time[2] << 8)
+                    | (time[3]))
+        tv_usec = ((time[4] << 24)
+                    | (time[5] << 16)
+                    | (time[6] << 8)
+                    | time[7])
+        dt = datetime.fromtimestamp(tv_sec)
+        return dt + timedelta(microseconds=tv_usec)
+
+
+    def parse(self):
         if self.content is None:
             self.__load_file()
 
         file_length = len(self.content)
         index = 0
 
-        data = []
         while index < file_length:
+            time = self.__decode_time(self.content[index:index+8])
+            index += 8
             key = self.content[index]
-
             length = self.content[index+1]
             cls = TMtcDecoder._class_map.get(key, None)
             if cls is None:
@@ -178,13 +237,20 @@ class TMtcDecoder:
                 values.append(value)
             index += length * 8
 
-            inst = cls(*values)
-            data.append(inst)
-            print(inst)
+            inst = cls(time, *values)
+            yield inst
 
-        return data
+    def decode(self):
+        allocations = self.parse()
+        addresses = {}
+        while (curr := next(allocations, None)) is not None:
+            if curr.ptr not in addresses:
+                addresses[curr.ptr] = [curr]
+            else:
+                addresses[curr.ptr].append(curr)
 
+        pprint(addresses)
 
-decoder = TMtcDecoder("../memory-tracker/memory_tracker.tmtc")
-d = decoder.decode()
-print(d)
+decoder = TMtcDecoder("memory_tracker.tmtc")
+decoder.decode()
+
