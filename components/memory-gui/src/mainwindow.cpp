5a9126c2e88b776f50575344f53f92b965b12d30
Duncan Jones <dj223@outlook.com>
2025-01-02 15:38:28 +0000

    Added loading bar to GUI

--- components/memory-gui/src/mainwindow.cpp
+++ components/memory-gui/src/mainwindow.cpp
@@ -2,18 +2,28 @@
 #include "mainwindow.h"
 #include "ui/ui_mainwindow.h"
 #include "QPushButton"
+#include <QDesktopServices>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
 
-    connect(ui->myButton, &QPushButton::clicked, this, [this]() {
-        ui->myButton->setText("Button Clicked!");
-    });
+    connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::loadButtonClick);
 }
 
 
 MainWindow::~MainWindow() {
     delete ui;
+}
+
+
+void MainWindow::loadButtonClick() {
+    QString filePath = QFileDialog::getOpenFileName(this, "Open File", "", "Memory Time Encoding (*.mtc);;All Files (*)");
+    if (!filePath.isEmpty()) {
+        qDebug() << "Selected file:" << filePath;
+    }else {
+        qDebug() << "No file selected";
+    }
 }
 
 
