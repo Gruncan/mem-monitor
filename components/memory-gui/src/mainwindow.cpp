805cdec4ed7ff225f6ee45854ae4b69777cf118a
Duncan Jones <dj223@outlook.com>
2025-01-02 19:56:25 +0000

    Added data plotting on load

--- components/memory-gui/src/mainwindow.cpp
+++ components/memory-gui/src/mainwindow.cpp
@@ -35,22 +35,7 @@ MainWindow::MainWindow(QWidget *parent) :
     decoderThread->start();
     monitorThread->start();
 
-    QVector<double> x(101), y(101); // initialize with entries 0..100
-    for (int i=0; i<101; ++i)
-    {
-        x[i] = i/50.0 - 1; // x goes from -1 to 1
-        y[i] = x[i]*x[i]; // let's plot a quadratic function
-    }
-    // create graph and assign data to it:
-    ui->plot->addGraph();
-    ui->plot->graph(0)->setData(x, y);
-    // give the axes some labels:
-    ui->plot->xAxis->setLabel("x");
-    ui->plot->yAxis->setLabel("y");
-    // set axes ranges, so we see all data:
-    ui->plot->xAxis->setRange(-1, 1);
-    ui->plot->yAxis->setRange(0, 1);
-    ui->plot->replot();
+
 
 }
 
@@ -89,6 +74,22 @@ void MainWindow::loadButtonClick() {
 void MainWindow::loaded() {
     qDebug() << "Loaded";
     ui->progressBar->setValue(100);
+    QVector<double> x(101), y(101); // initialize with entries 0..100
+    for (int i=0; i<101; ++i)
+    {
+        x[i] = i/50.0 - 1; // x goes from -1 to 1
+        y[i] = x[i]*x[i]; // let's plot a quadratic function
+    }
+    // create graph and assign data to it:
+    ui->plot->addGraph();
+    ui->plot->graph(0)->setData(x, y);
+    // give the axes some labels:
+    ui->plot->xAxis->setLabel("x");
+    ui->plot->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    ui->plot->xAxis->setRange(-1, 1);
+    ui->plot->yAxis->setRange(0, 1);
+    ui->plot->replot();
     // monitorThread->terminate();
     // decoderThread->terminate();
 };
