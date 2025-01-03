ee459a31f71da0c62668d13488f2f8895047ae7a
Duncan Jones <dj223@outlook.com>
2025-01-03 14:56:39 +0000

    Added simple tree selector

---
+++ components/memory-gui/include/plotdata.h
@@ -0,0 +1,20 @@
+
+#ifndef PLOTDATA_H
+#define PLOTDATA_H
+#include <QList>
+#include <QString>
+
+
+struct PlotData {
+    QString name;
+    bool enabled = false;
+    QVector<double> x;
+    QVector<double> y;
+};
+
+struct PlotCategory {
+    QString name;
+    std::vector<PlotData> data;
+};
+
+#endif //PLOTDATA_H
