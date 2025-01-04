cbae04524365df67bbc2d084ef999aec73c6fcaa
Duncan Jones <dj223@outlook.com>
2025-01-04 15:58:43 +0000

    Fixed memory length bug

--- components/common/mtc/src/mtc-object.cpp
+++ components/common/mtc/src/mtc-object.cpp
@@ -7,30 +7,30 @@ namespace mtc {
 
     }
 
-    inline void MtcObject::add_point(const uint8_t key, MtcPoint* point) {
+    void MtcObject::add_point(const uint8_t key, MtcPoint* point) {
         if (!points_maps.contains(key)) {
-            points_maps[key] = new std::vector<MtcPoint*>(length + 1);
+            points_maps[key] = new std::vector<MtcPoint*>();
         }
         points_maps[key]->push_back(point);
     }
 
-    inline int MtcObject::get_version() const {
+    int MtcObject::get_version() const {
         return version;
     }
 
-    inline std::tm MtcObject::get_date() const {
+    std::tm MtcObject::get_date() const {
         return date;
     }
 
-    inline std::vector<MtcPoint*>* MtcObject::get_points(const uint8_t key){
+    std::vector<MtcPoint*>* MtcObject::get_points(const uint8_t key){
         return points_maps[key];
     }
 
-    inline uint64_t MtcObject::get_length() const {
+    uint64_t MtcObject::get_length() const {
         return length;
     }
 
-    inline void MtcObject::add_time(uint16_t* time) {
+    void MtcObject::add_time(uint16_t* time) {
         time_pointers.push_back(time);
         length++;
     }
