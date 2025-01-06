67f17ac0097802685db3f3a08a2459cc29e2fe72
Duncan Jones <dj223@outlook.com>
2025-01-06 18:46:22 +0000

    Updated mtc decoder to check if parameters are actually used

--- components/common/mtc/src/mtc-object.cpp
+++ components/common/mtc/src/mtc-object.cpp
@@ -4,12 +4,16 @@
 namespace mtc {
 
     MtcObject::MtcObject(int version, std::tm timestamp): version(version), date(timestamp), length(0) {
-
+        point_default = new std::map<uint8_t, bool>();
     }
 
     void MtcObject::add_point(const uint8_t key, MtcPoint* point) {
         if (!points_maps.contains(key)) {
             points_maps[key] = new std::vector<MtcPoint*>();
+            (*point_default)[key] = true;
+        }
+        if (point->value != 0) {
+            (*point_default)[key] = false;
         }
         points_maps[key]->push_back(point);
     }
@@ -26,6 +30,14 @@ namespace mtc {
         return points_maps[key];
     }
 
+    bool MtcObject::is_point_default(const uint8_t key) {
+        return point_default->at(key);
+    }
+
+    std::map<uint8_t, bool>* MtcObject::get_default_points() const {
+        return point_default;
+    }
+
     uint64_t MtcObject::get_length() const {
         return length;
     }
@@ -34,6 +46,8 @@ namespace mtc {
         time_pointers.push_back(time);
         length++;
     }
+
+
 
 
 }
