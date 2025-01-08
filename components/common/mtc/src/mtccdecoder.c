f815b4c3fc437871765779ad30f5bab3a652344f
Duncan Jones <dj223@outlook.com>
2025-01-08 20:23:58 +0000

    Drastically improved decoder speed and memory usage

---
+++ components/common/mtc/src/mtccdecoder.c
@@ -0,0 +1,150 @@
+
+#include "mtccdecoder.h"
+
+#include "../../mem-monitor-config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HEADER_SIZE 5
+#define KEY_SIZE 225
+#define INIT_SIZE 5120
+
+const static uint16_t CHUNK_SIZE = 679;
+
+typedef unsigned char byte;
+
+inline static void initalise(struct MtcObject* object) {
+    object->point_map = malloc(sizeof(struct MtcPointMap) * KEY_SIZE);
+    object->alloc_size_points = INIT_SIZE;
+    for (mk_size_t i = 0; i < KEY_SIZE; i++) {
+        object->point_map[i].points = malloc(sizeof(struct MtcPoint) * object->alloc_size_points);
+        object->point_map[i].points[0].repeated = 0;
+        object->point_map[i].points[0].time_offset = malloc(sizeof(uint16_t));
+        object->point_map[i].length = 0;
+    }
+    object->alloc_size_times = INIT_SIZE;
+    object->times = malloc(sizeof(struct MtcTime) * object->alloc_size_times);
+    object->times[0].repeated = 0;
+    object->times[0].time_offset = malloc(sizeof(uint16_t));
+    object->length = 0;
+    object->version = 0;
+}
+
+inline static void decode_header(const byte* buffer, struct MtcObject* object) {
+    object->version = buffer[0];
+    //todo add time decoding here
+}
+
+static void decode_chunk(const byte* buffer, struct MtcObject* object) {
+    if (object == NULL) {
+        return;
+    }
+
+    const uint16_t time_offset = buffer[0] << 8 | buffer[1];
+    if (object->length == object->alloc_size_times) {
+        object->alloc_size_times *= 2;
+        void* new_ptr = realloc(object->times, object->alloc_size_times * sizeof(struct MtcTime));
+        if (new_ptr == NULL) {
+            perror("Failed to realloc point times");
+        }
+        object->times = new_ptr;
+    }
+    if (object->length == 0) {
+        *object->times[0].time_offset = time_offset;
+        object->length++;
+    } else {
+        if (*object->times[object->length-1].time_offset == time_offset) {
+            object->times[object->length-1].repeated++;
+        } else {
+            object->times[object->length].time_offset = malloc(sizeof(uint16_t));
+            *object->times[object->length].time_offset = time_offset;
+
+            object->times[object->length].repeated = 0;
+            object->length++;
+        }
+    }
+    const uint16_t length_offset = buffer[2] << 8 | buffer[3];
+
+    for (uint16_t i = 4; i < length_offset + 4; i += 3) {
+        const mk_size_t key = buffer[i];
+        const uint16_t value = buffer[i + 1] << 8 | buffer[i + 2];
+        if (object->point_map[key].length == object->alloc_size_points) {
+            object->alloc_size_points *= 2;
+            for (mk_size_t j = 0; j < KEY_SIZE; j++) {
+                void* new_ptr =  realloc(object->point_map[j].points, object->alloc_size_points * sizeof(struct MtcPoint));
+                if (new_ptr == NULL) {
+                    perror("Failed to realloc point map");
+                }
+                object->point_map[j].points = new_ptr;
+            }
+
+        }
+
+        if (object->point_map[key].length == 0) {
+            object->point_map[key].points[0].time_offset = object->times[object->length].time_offset;
+            object->point_map[key].points[0].value = value;
+            object->point_map[key].length++;
+        } else {
+            const uint16_t* length = &object->point_map[key].length;
+            if (object->point_map[key].points[*length-1].value == value) {
+                object->point_map[key].points[*length-1].repeated++;
+                object->point_map[key].length++;
+            }else {
+                object->point_map[key].points[*length].time_offset = object->times[object->length].time_offset;
+                object->point_map[key].points[*length].value = value;
+                object->point_map[key].points[*length].repeated = 0;
+                object->point_map[key].length++;
+            }
+        }
+    }
+}
+
+
+struct MtcObject* decode(const char* filename) {
+    FILE* fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        perror("Failed to open file!");
+        return NULL;
+    }
+
+    byte* buffer = malloc(CHUNK_SIZE);
+    if (buffer == NULL) {
+        perror("Failed to allocate buffer!");
+        fclose(fp);
+        return NULL;
+    }
+
+
+    struct MtcObject* object = malloc(sizeof(struct MtcObject));
+    initalise(object);
+
+    size_t bytesRead = 0;
+    bytesRead = fread(buffer, 1, HEADER_SIZE, fp);
+
+    if (bytesRead != HEADER_SIZE) {
+        perror("Failed to read header!");
+        fclose(fp);
+    }
+
+    decode_header(buffer, object);
+
+    int offset = 0;
+    while ((bytesRead = fread(buffer, 1, CHUNK_SIZE, fp)) > 0) {
+        printf("%d\n", offset);
+        decode_chunk(buffer, object);
+        offset++;
+    }
+
+    free(buffer);
+    fclose(fp);
+    return object;
+}
+
+
+int main() {
+    struct MtcObject* object = decode("/home/duncan/Desktop/uwb_test1.mtc");
+    printf("%d\n", object->length);
+
+    return 0;
+}
--- components/common/mtc/tests/mtc-tests.cpp
+++ components/common/mtc/tests/mtc-tests.cpp
@@ -1,11 +1,29 @@
 
 
 
+#include <fstream>
 #include <iostream>
 #include <memory-decoder.h>
 #include <mtc-decoder.h>
 
 using namespace mtc;
+
+void printMemoryUsage() {
+    std::ifstream statusFile("/proc/self/status");
+    std::string line;
+
+    if (!statusFile.is_open()) {
+        std::cerr << "Error: Unable to open /proc/self/status." << std::endl;
+        return;
+    }
+
+    while (std::getline(statusFile, line)) {
+        if (line.find("VmRSS:") == 0 || line.find("VmSize:") == 0) {
+            std::cout << line << std::endl;
+        }
+    }
+    statusFile.close();
+}
 
 int main() {
     auto start = std::chrono::high_resolution_clock::now();
@@ -15,6 +33,7 @@ int main() {
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
     std::cout << duration.count() << " seconds" << std::endl;
-
+    std::cout << object->get_length() << std::endl;
+    printMemoryUsage();
     return 0;
 }
