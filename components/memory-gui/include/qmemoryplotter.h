339017ad26f531397383fd2977f5a989e18c3a4e
Duncan Jones <dj223@outlook.com>
2025-01-09 23:00:26 +0000

    Added axis controls to UI

--- components/memory-gui/include/qmemoryplotter.h
+++ components/memory-gui/include/qmemoryplotter.h
@@ -8,20 +8,31 @@
 #include "mtccdecoder.h"
 
 
-class QMemoryPlotter : public QObject {
+class QMemoryPlotter : public QWidget {
     Q_OBJECT
 
 public:
 
-    explicit QMemoryPlotter(QCustomPlot* plot, QMtcLoader* loader);
+    explicit QMemoryPlotter(QWidget* parent, QCustomPlot* plot, QMtcLoader* loader);
 
     void addPlot(mk_size_t key);
 
     void removePlot(mk_size_t key);
 
+    void setupSpinBox(QDoubleSpinBox* spinBox);
+
+    void updateInputsFromPlot();
+
+    void setSpinBoxRanges(double xMin, double xMax, double yMin, double yMax);
+
 public Q_SLOTS:
 
     void plotToggleChange(const QString& category, const QString& plotString, bool enabled);
+
+    void updateXRange();
+
+    void updateYRange();
+
 
 Q_SIGNALS:
     void queueRendering(MtcPointMap* points, const QVector<double>& times, uint64_t length, QCPGraph* graph);
@@ -41,8 +52,12 @@ private:
     std::mt19937 gen;
     std::vector<QColor> usedColors;
 
-
     QColor generateRandomColor();
+
+    QDoubleSpinBox* xMinInput;
+    QDoubleSpinBox* xMaxInput;
+    QDoubleSpinBox* yMinInput;
+    QDoubleSpinBox* yMaxInput;
 
 };
 
