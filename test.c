e29fa3d2268fd802ee90b9795f28e4a48ef38203
Duncan Jones <dj223@outlook.com>
2025-01-21 14:39:00 +0000

    Started on test writing for TMTC decoder

--- test.c
+++ test.c
@@ -4,29 +4,30 @@
 #include <stdio.h>
 
 int main(){
-    printf("%lu\n", sizeof(size_t));
-    printf("%lu\n", sizeof(long));
-    printf("%lu\n", sizeof(unsigned int));
-    printf("%lu\n", sizeof(long unsigned int));
-    printf("%lu\n", sizeof(unsigned char));
-    unsigned long long* ptr = malloc(1024);
+//    printf("%lu\n", sizeof(size_t));
+//    printf("%lu\n", sizeof(long));
+//    printf("%lu\n", sizeof(unsigned int));
+//    printf("%lu\n", sizeof(long unsigned int));
+//    printf("%lu\n", sizeof(unsigned char));
+//    unsigned long long* ptr = malloc(1024);
+//
+//    printf("Allocated 128 at %p\n", ptr);
+//
+//    printf("Freeing %p\n", ptr);
+//    free(ptr);
+//
+//    ptr[1] = 1234;
+//
+//    printf("Freeing again %p\n", ptr);
+//    free(ptr);
+//
+//    void* next_malloc1 = malloc(12);
+//    void* next_malloc2 = malloc(12);
+//
+//    printf("Malloc 1 %p\n", next_malloc1);
+//    printf("Malloc 2 %p\n", next_malloc2);
+//
+//    void* next_malloc3 = malloc(12);
+//    printf("Malloc 2 %p\n", next_malloc2);#
 
-    printf("Allocated 128 at %p\n", ptr);
-
-    printf("Freeing %p\n", ptr);
-    free(ptr);
-
-    ptr[1] = 1234;
-
-    printf("Freeing again %p\n", ptr);
-    free(ptr);
-
-    void* next_malloc1 = malloc(12);
-    void* next_malloc2 = malloc(12);
-
-    printf("Malloc 1 %p\n", next_malloc1);
-    printf("Malloc 2 %p\n", next_malloc2);
-
-    void* next_malloc3 = malloc(12);
-    printf("Malloc 2 %p\n", next_malloc2);
 }
