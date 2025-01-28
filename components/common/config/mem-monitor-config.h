26eeeb98c83069fc06fe156f474388456eba16b6
Duncan Jones <dj223@outlook.com>
2025-01-28 11:43:31 +0000

    Moved debug format strings into config file

--- components/common/config/mem-monitor-config.h
+++ components/common/config/mem-monitor-config.h
@@ -27,21 +27,23 @@
 
 #define MALLOC_FORMAT_STR "malloc(%zd) = %p\n"
 #define CALLOC_FORMAT_STR "calloc(%zd, %zd) = %p\n"
-#define REALLOC_FORMAT_STR
-#define REALLOC_ARRAY_FORMAT_STR
-#define FREE_FORMAT_STR
-#define NEW_FORMAT_STR
-#define NEW_NOTHROW_FORMAT_STR
-#define NEW_ARRAY_FORMAT_STR
-#define NEW_ARRAY_NOTHROW_FORMAT_STR
-#define DELETE_FORMAT_STR
-#define DELETE_SIZED_FORMAT_STR
-#define DELETE_NOTHROW_FORMAT_STR
-#define DELETE_ARRAY_FORMAT_STR
-#define DELETE_ARRAY_SIZED_FORMAT_STR
-#define DELETE_ARRAY_NOTHROW_FORMAT_STR
-#define NEW_ALIGN_FORMAT_STR
-
+#define REALLOC_FORMAT_STR "reallocarray(%p, %zd, %zd) = %pan\n"
+#define REALLOC_ARRAY_FORMAT_STR "realloc(%p, %zd) = %p\n"
+#define FREE_FORMAT_STR "free(%p)\n"
+#define NEW_FORMAT_STR "new(%zd) = %p\n"
+#define NEW_NOTHROW_FORMAT_STR "new_nothrow(%zd) = %p\n"
+#define NEW_ARRAY_FORMAT_STR "new[](%zd) = %p\n"
+#define NEW_ARRAY_NOTHROW_FORMAT_STR "new_nothrow[](%zd) = %p\n"
+#define DELETE_FORMAT_STR "delete(%p)\n"
+#define DELETE_SIZED_FORMAT_STR "delete_sized(%p, %lu)\n"
+#define DELETE_NOTHROW_FORMAT_STR "delete_nothrow(%p)\n"
+#define DELETE_ARRAY_FORMAT_STR "delete[](%p)\n"
+#define DELETE_ARRAY_SIZED_FORMAT_STR "delete[](%p, %zd)\n"
+#define DELETE_ARRAY_NOTHROW_FORMAT_STR "delete_nothrow[](%p)\n"
+#define NEW_ALIGN_FORMAT_STR "new_align(%zd, %zd) = %p\n"
+#define NEW_ARRAY_ALIGN_FORMAT_STR "new[]_align(%zd, %zd) = %p\n"
+#define DELETE_ALIGN_FORMAT_STR "delete_align(%p, %zd)\n"
+#define DELETE_ARRAY_ALIGN_FORMAT_STR "delete_align[](%p, %zd)\n"
 
 // Memory key size
 typedef uint8_t mk_size_t;
