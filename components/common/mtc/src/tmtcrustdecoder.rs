eaac69d06a7c5ce954cfaa6697bd4a68a6e642be
Duncan Jones <dj223@outlook.com>
2025-04-03 15:39:58 +0100

    Move updates to tmtc

--- components/common/mtc/src/tmtcrustdecoder.rs
+++ components/common/mtc/src/tmtcrustdecoder.rs
@@ -1,11 +1,16 @@
-use crate::{c_createTMtcObject, mtc_point_size_t, CMtcObject, CTMtcObject};
+use crate::{c_createTMtcObject, c_destroyTMtcObject, mtc_point_size_t, CMtcObject, CTMtcObject, CTMtcPoint};
 use crate::mtcrustdecoder::TMtcObject;
 
 
 
 pub struct TMtcObject {
     raw: CTMtcObject,
+    values: Vec<TMtcPoint>,
     owns_memory: bool,
+}
+
+pub struct TMtcPoint {
+    raw: CTMtcPoint,
 }
 
 
@@ -26,7 +31,43 @@ impl TMtcObject {
 
         TMtcObject{
             raw,
-            owns_memory: true
+            owns_memory: true,
+            values: {
+                let mut vec = Vec::with_capacity(raw.size as usize);
+                for i in 0..raw.size {
+                    vec.push(TMtcPoint {raw: &mut raw.points[i as usize]});
+                }
+                vec
+            }
+        }
+    }
+
+    pub fn get_size(&self) -> usize {
+        self.raw.size as usize
+    }
+
+    pub fn get_is_collapsable(&self) -> bool {
+        if self.raw.is_collapsable {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn get_points(&self) -> Vec<CTMtcPoint> {
+        for i in 0..self.get_size() {
+            TMtcPoint {raw: self.raw.points[i as usize]}
+        }
+    }
+
+}
+
+impl Drop for TMtcObject {
+    fn drop(&mut self) {
+        if self.owns_memory {
+            unsafe {
+                c_destroyTMtcObject(&mut self.raw);
+            }
         }
     }
 }
